d0d4c73ba50191515df513bee1b2c720afb9a207
AshkanParsafar <161747971+ashkan-parsafar@users.noreply.github.com>
2024-08-15 21:38:16 +0330

    Update mini project.py

--- week1/mini project.py
+++ week1/mini project.py
@@ -1,3 +1,10 @@
 s = "a dictionary python is datastructure and it is in method dictionary of python language"
 h=s.split()
-print(h)
+d={}
+for i in h:
+    if i in d:
+        d[i]+=1
+    else:
+        d[i]=1
+
+print(d)
